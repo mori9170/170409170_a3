39e13fd08a4a929e985d0c1361c74744d74e01cf
Michael Moriarty 😎 <Michael@Michaels-MacBook-Pro-4.local>
2019-11-10 18:46:31 -0500

    first commit

---
+++ q1/www/js/app.js
@@ -0,0 +1,13 @@
+
+  function newdiv(displaydiv,div2,div3,div4){
+    displaydiv = document.getElementById(displaydiv);
+    div2 = document.getElementById(div2);
+    div3 = document.getElementById(div3);
+    div4 = document.getElementById(div4);
+
+    displaydiv.style.display = 'flex';
+    div2.style.display = 'none';
+    div3.style.display = 'none';
+    div4.style.display = 'none';
+
+  }
---
+++ q3/www/js/app.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+  function validateEmail() {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var email = document.forms["chimpForm"]["email"].value;
+    if(email.match(mailformat)) {
+      return true;
+    }
+    else {
+      alert("Invalid Email");
+      return false;
+    }
+
+  }
+
+
+  // function validateUser() {
+  //   var userformat = /^[a-zA-Z][^\s]{7,23}$/;
+  //   var usertest = document.forms["chimpForm"]["username"].value;
+  //   if(usertest.match(userformat)) {
+  //     return true;
+  //   }
+  //   else {
+  //     alert('Invalid Username');
+  //     return false;
+  //   }
+  // }
+
+// Make sure to check for characters over 24 still working in the regex for some reason
+
+  // function validatePass() {
+  //   var passformat = /^[a-zA-Z][^\s]{7,23}$/;
+  //   var passtest = document.forms["chimpForm"]["password"].value;
+  //   if(passtest.match(passformat)) {
+  //     return true;
+  //   }
+  //   else {
+  //     alert('Invalid Password');
+  //     return false;
+  //   }
+  // }
+  //
+  //
+  //
+  //
+  // function passShow() {
+  //   var x = document.getElementById('pass');
+  //   if (x.type === "password") {
+  //     x.type = "text";
+  //   }
+  //   else {
+  //     x.type= "password";
+  //   }
+  // }
+
+
+
+
+  // function checkform()
+  //   {
+  //       var f = document.forms["chimpForm"].elements;
+  //       var cansubmit = true;
+  //
+  //       for (var i = 0; i < f.length; i++) {
+  //           if (f[i].value.length == 0) cansubmit = false;
+  //       }
+  //
+  //       if (cansubmit) {
+  //           document.getElementById('submitbutton').disabled = false;
+  //       }
+  //   }
